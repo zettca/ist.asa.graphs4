ca0d2848fc885a1870f947a0bd11af19deffed12
Bruno Henriques <zettca@gmail.com>
2017-04-27 03:12:03 +0100

    Implement first versionee

--- main.cpp
+++ main.cpp
@@ -2,27 +2,18 @@
 #include <vector>
 #include <algorithm>
 
-#define FAIL "Insuficiente"
-
-class Buildable {
+class City {
 public:
-    unsigned int cost;
+    int id;
     bool built = false;
 
-    Buildable(unsigned int c) : cost(c) {}
+    City(int i) : id(i) {}
 };
 
-class City : public Buildable {
-public:
-    unsigned int id;
-    bool reachable = false;
-
-    City(unsigned int i) : Buildable(0), id(i) {}
-};
-
-class Road : public Buildable {
+class Road {
 public:
     City *city1, *city2;
+    int cost;
     bool virt = false;
 
     void build() {
@@ -30,48 +21,42 @@ public:
       city2->built = true;
     }
 
-    Road(City *c1, City *c2, unsigned int c) : Road(c1, c2, c, false) {};
+    Road(City *c1, City *c2, int c) : Road(c1, c2, c, false) {};
 
-    Road(City *c1, City *c2, unsigned int c, bool v) : Buildable(c), city1(c1), city2(c2), virt(v) {};
+    Road(City *c1, City *c2, int c, bool v) : city1(c1), city2(c2), cost(c), virt(v) {};
 };
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  unsigned int N, A, E; /* Cities, Airports, Edges/Paths */
-  unsigned int a, b, c;
+  int N, A, E; /* Cities, Airports, Edges/Paths */
+  int a, b, c;
   vector<City *> cities;
   vector<Road *> roads;
 
   /* INPUTING */
 
   cin >> N;
-  for (unsigned int i = 0; i <= N; i++) {
+  for (int i = 0; i <= N; i++) {
     cities.push_back(new City(i));
   }
   City *sky = cities.at(0);
 
   cin >> A;
-  cout << "Reading " << A << " Airports" << endl;
-  for (unsigned int i = 0; i < A; i++) {
+  //cout << "Reading " << A << " Airports" << endl;
+  for (int i = 0; i < A; i++) {
     cin >> a >> c;
-    City *city = cities.at(a);
-    city->cost = c;
-    city->reachable = true;
-
-    roads.push_back(new Road(city, sky, c, true));
+    roads.push_back(new Road(cities.at(a), sky, c, true));
   }
 
   cin >> E;
-  cout << "Reading " << E << " Streets" << endl;
-  for (unsigned int i = 0; i < E; i++) {
+  //cout << "Reading " << E << " Streets" << endl;
+  for (int i = 0; i < E; i++) {
     cin >> a >> b >> c;
     roads.push_back(new Road(cities.at(a), cities.at(b), c));
   }
 
   /* ALGORITHMING */
-
-  cout << "ALGORITHMING... !!!" << endl;
 
   sort(roads.begin(), roads.end(), [](Road *r1, Road *r2) {
       return (r1->cost == r2->cost) ? (!r1->virt) : r1->cost < r2->cost;
@@ -85,7 +70,14 @@ int main(int argc, char const *argv[]) {
     }
   }
 
-  unsigned int cost = 0, nAir = 0, nRoads = 0;
+  for (auto city : cities) {
+    if (!city->built){
+      cout << "Insuficiente" << endl;
+      return 1;
+    }
+  }
+
+  int cost = 0, nAir = 0, nRoads = 0;
   for (auto m : mst) {
     cost += m->cost;
     if (m->virt) nAir++;
